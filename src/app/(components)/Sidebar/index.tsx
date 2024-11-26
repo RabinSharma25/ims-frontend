cfa3d9822db3a06035df5a22a9b5fdfa692da4ad
RabinSharma25 <cruzosharmas22545@gmail.com>
2024-11-26 22:06:33 +0530

    Added redux setup files

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+"use client";
+import { Menu } from "lucide-react";
+import React from "react";
 
-type Props = {}
-
-const Sidebar = (props: Props) => {
+const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div>
+      {/* Top Logo */}
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>logo</div>
 
-export default Sidebar
+        <h1 className="font-extrabold text-2xl ">GoStock</h1>
+
+        <button
+          className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
+          onClick={() => {}}
+        >
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+      {/* Links */}
+      <div className="flex-grow mt-8"> {/* Links here */}</div>
+      {/* Footer */}
+      <div>
+        <p className="text-center text-xs text-gray-500">&copy; 2024 GoStock</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -4,7 +4,7 @@ import Navbar from "@/app/(components)/Navbar"
 import Sidebar from "@/app/(components)/Sidebar"
 const DashboardWrapper = ({children}: {children:React.ReactNode}) => {
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+    <div className={` light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
       <Sidebar/>
     <main className={` flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}> 
     <Navbar/>
@@ -13,5 +13,4 @@ const DashboardWrapper = ({children}: {children:React.ReactNode}) => {
 
   )
 }
-
 export default DashboardWrapper
