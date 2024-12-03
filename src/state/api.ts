d97d593ed2ec113a0a277075661079c26a0baf3f
RabinSharma25 <cruzosharmas22545@gmail.com>
2024-12-04 00:10:22 +0530

    Made expenses page

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,140 +1,3 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// import { Interface } from "readline";
-
-// // Define your interfaces for the data
-// export interface Product {
-//   id: string;
-//   name: string;
-//   price: number;
-//   rating?: number;
-//   stockQuantity: number;
-// }
-
-// export interface NewProduct {
-//   name: string;
-//   price: number;
-//   rating?: number;
-//   stockQuantity: number;
-// }
-
-// export interface SalesSummary {
-//   salesSummaryId: string;
-//   totalValue: number;
-//   changePercentage?: number;
-//   date: string;
-// }
-
-// export interface PurchaseSummary {
-//   purchaseSummaryId: string;
-//   totalPurchased: number;
-//   changePercentage?: number;
-//   date: string;
-// }
-
-// export interface ExpenseSummary {
-//   expenseSummarId: string;
-//   totalExpenses: number;
-//   date: string;
-// }
-
-// export interface ExpenseByCategorySummary {
-//   expenseByCategorySummaryId: string;
-//   category: string;
-//   amount: string;
-//   date: string;
-// }
-
-// export interface User {
-//   id: string;
-//   name: string;
-//   email: string;
-// }
-
-// export interface DashboardData {
-//   ProductList: Product[];
-//   SalesSummaryList: SalesSummary[];
-//   PurchaseSummaryList: PurchaseSummary[];
-//   ExpenseSummaryList: ExpenseSummary[];
-//   ExpenseByCategoryList: ExpenseByCategorySummary[];
-// }
-// export interface DashboardApiResponse {
-//   code: number;
-//   success: boolean;
-//   status: string;
-//   message: string;
-//   data: DashboardData[];
-// }
-
-// export interface Products{
-//   ProductList:Product[]
-// }
-// export interface ProductApiResponse{
-//   code: number;
-//   success: boolean;
-//   status: string;
-//   message: string;
-//   data: Products;
-// }
-
-// export interface Users{
-//   UserList:User[]
-// }
-// export interface UserApiResponse{
-//   code: number;
-//   success: boolean;
-//   status: string;
-//   message: string;
-//   data: Users
-// }
-
-
-
-// // Set up the API service
-// export const api = createApi({
-//     baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
-//     reducerPath: "api",
-//     tagTypes: ["DashboardApiResponse", "ProductApiResponse", "UserApiResponse", "Expenses"],
-//     endpoints: (build) => ({
-//       getDashboardMetrics: build.query<DashboardApiResponse, void>({
-//         query: () => "/dashboard/getDashboardMetrices", // Ensure this is the correct API endpoint
-//         providesTags: ["DashboardApiResponse"],
-//       }),
-
-//       getProducts: build.query<ProductApiResponse, string | void>({
-//         query: (search) => ({
-//           url: "/product/getProducts",
-//           params: search ? { search } : {},
-//         }),
-//         providesTags: ["ProductApiResponse"],
-//       }),
-//       createProduct: build.mutation<Product, NewProduct>({
-//         query: (newProduct) => ({
-//           url: "/products",
-//           method: "POST",
-//           body: newProduct,
-//         }),
-//         invalidatesTags: ["ProductApiResponse"],
-//       }),
-//       getUsers: build.query<UserApiResponse, void>({
-//         query: () => "/user/getUsers",
-//         providesTags: ["UserApiResponse"],
-//       }),
-//       getExpensesByCategory: build.query<ExpenseByCategorySummary[], void>({
-//         query: () => "/expenses",
-//         providesTags: ["Expenses"],
-//       }),
-//     }),
-//   });
-  
-
-//   export const {
-//     useGetDashboardMetricsQuery,
-//     useGetProductsQuery,
-//     useCreateProductMutation,
-//     useGetUsersQuery,
-//     useGetExpensesByCategoryQuery,
-//   } = api;
-
 
 
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
@@ -177,7 +40,7 @@ export interface ExpenseSummary {
 }
 
 export interface ExpenseByCategorySummary {
-  expenseByCategorySummaryId: string;
+  id: string;
   category: string;
   amount: string;
   date: string;
@@ -237,6 +100,20 @@ export interface AddProductApiResponse {
   data: Product;
 }
 
+export interface ExByCategory {
+  ExpenseList: ExpenseByCategorySummary[];
+}
+
+export interface ExpenseApiResponse {
+  code: number;
+  success: boolean;
+  status: string;
+  message: string;
+  data: ExByCategory;
+}
+
+
+
 // Set up the API service
 export const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
@@ -245,7 +122,7 @@ export const api = createApi({
     "DashboardApiResponse",
     "ProductApiResponse",
     "UserApiResponse",
-    "Expenses",
+    "ExpenseApiResponse",
     "AddProductApiResponse",
   ],
   endpoints: (build) => ({
@@ -271,9 +148,9 @@ export const api = createApi({
     }),
 
     // Fetch Expenses by Category
-    getExpensesByCategory: build.query<ExpenseByCategorySummary[], void>({
-      query: () => "/expenses",
-      providesTags: ["Expenses"],
+    getExpensesByCategory: build.query<ExpenseApiResponse, void>({
+      query: () => "/expense/getExpensesByCategory",
+      providesTags: ["ExpenseApiResponse"],
     }),
 
     // Add Product - Separate Endpoint
